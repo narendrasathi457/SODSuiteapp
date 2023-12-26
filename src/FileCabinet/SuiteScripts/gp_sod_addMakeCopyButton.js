86070c7a2c8ac66618a61dffead4841186222728
narendra-enquero-457 <94597711+narendra-enquero-457@users.noreply.github.com>
2023-12-26 23:26:03 +0530

    first version with hardcoded stuff

--- src/FileCabinet/SuiteScripts/gp_sod_addMakeCopyButton.js
+++ src/FileCabinet/SuiteScripts/gp_sod_addMakeCopyButton.js
@@ -6,50 +6,31 @@ define(['N/record', 'N/log', 'N/ui/serverWidget','N/url'],
     function(record, log, serverWidget,url) {
 
         function beforeLoad(context) {
-            // Check if the record is in view mode (not in edit mode
            try{
            var currentRecord=context.newRecord;
-             var defaultPolicy=currentRecord.getValue('custrecord_gpsod_defaultpolicy');
-
-               // Get the current form
-                var form = context.form;
-
-                           var defaultPolicyField = form.getField({
-    id : 'custrecord_gpsod_defaultpolicy'
-});
-               defaultPolicyField.updateDisplayType({
-    displayType : serverWidget.FieldDisplayType.HIDDEN
-});
-
-             
-
+           var defaultPolicy=currentRecord.getValue('custrecord_gp_sod_defaultpolicy');
+           var form = context.form;
+           var defaultPolicyField = form.getField({id : 'custrecord_gp_sod_defaultpolicy'});
+           defaultPolicyField.updateDisplayType({displayType : serverWidget.FieldDisplayType.HIDDEN});
              if(defaultPolicy)
              {
-              
-
-                // Add a button to the form
-
                var suitelet_Url = url.resolveScript({
-                scriptId: 'customscript_sod_makecopy_sl',
-                deploymentId: 'customdeploy_sod_makecopy_sl',
+                scriptId: 'customscript_gp_sod_makecopy_sl',
+                deploymentId: 'customdeploy_gp_sod_makecopy_sl',
                 returnExternalUrl: false
             });
 
-            suitelet_Url=suitelet_Url+'&recordId='+currentRecord.id
+            suitelet_Url=suitelet_Url+'&recordId='+currentRecord.id;
+
+                 form.removeButton({id :'edit'});  // Remove the standard edit button and add custom copy button
               
-                var copyButton = form.addButton({
+               form.addButton({
                     id: 'custpage_copy_button',
                     label: 'Copy',
                     functionName: "window.open('"+suitelet_Url+"', 'PopupWindow', 'width=800,height=600,scrollbars=yes,resizable=yes');"
                 });
 
-               var Remove_Standard_Button=form.removeButton({
 
-               id :'edit',
-               });
-
-           
-               
              }
          }
          catch(error)
